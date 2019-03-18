65069bbfdeee55cfcba45661506318a3f4109578
un-install <elllipsse@gmail.com>
2019-03-18 15:50:10 +0200

    init

--- src/main/java/springdata/exceptions/DeleteException.java
+++
@@ -1,8 +0,0 @@
-package springdata.exceptions;
-
-public class DeleteException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-    public DeleteException(String s) {
-        super(s);
-    }
-}
